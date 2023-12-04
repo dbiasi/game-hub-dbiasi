9503aabe91ff0ac00c479284efa6c0944b2766c9
dbiasi <dbiasi594@gmail.com>
2023-12-04 16:44:49 -0300

    Bild the color mode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,19 @@
+// ThemeToggleButton.tsx
+import { HStack, useColorMode, Switch, Text } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      ></Switch>
+      <Text>Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
