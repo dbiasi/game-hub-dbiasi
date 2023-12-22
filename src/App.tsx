550dc2cbde998b0955077bccf592609c65dfe872
dbiasi <dbiasi594@gmail.com>
2023-12-22 11:39:23 -0300

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -36,7 +37,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar></NavBar>
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        ></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,16 @@ import logo from "../assets/logo.webp"; // importing logo from the assets folder
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     // HStack is a chakara ui component used to layout our items horizontaly.
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px"></Image>
-      <SearchInput></SearchInput>
+      <SearchInput onSearch={onSearch}></SearchInput>
       <ColorModeSwitch />
     </HStack>
   );
