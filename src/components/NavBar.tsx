5e4ca42e3ee4514a5d7e36fdf0f686b35d051e08
dbiasi <dbiasi594@gmail.com>
2023-12-21 23:11:10 -0300

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp"; // importing logo from the assets folder
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     // HStack is a chakara ui component used to layout our items horizontaly.
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px"></Image>
+      <SearchInput></SearchInput>
       <ColorModeSwitch />
     </HStack>
   );
