8c4ab38dbe862d719f79d75181970e4bc5a1cabb
dbiasi <dbiasi594@gmail.com>
2024-01-26 20:52:09 -0300

    Customize the theme to get darker grays

--- src/services/theme.ts
+++ src/services/theme.ts
@@ -14,7 +14,23 @@ const config: ThemeConfig = {
 
 // Use extendTheme to customize the default Chakra UI theme
 // by passing our configuration object
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#a0a0a0",
+      500: "#898989",
+      600: "#6c6c6c",
+      700: "#202020",
+      800: "#121212",
+      900: "#111",
+    },
+  },
+});
 
 // Export the customized theme as the default export of this module
 // This theme can be used to provide theming context to your React application
