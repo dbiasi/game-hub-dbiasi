23c71b08df61a0780def5b05bb4bcf2f91267aa7
dbiasi <dbiasi594@gmail.com>
2024-01-26 21:01:10 -0300

    clining the code 2

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
